06b7436d7668f84ff4a822b456166b0660978c92
pjg012 <pjg012@gmail.com>
2021-11-02 15:46:54 +0900

    Creating columns and rows

--- week 1-Jetpack Compose basics/app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
+++ week 1-Jetpack Compose basics/app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.jetpackcomposebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +24,7 @@ class MainActivity : ComponentActivity() {
             JetpackComposeBasicTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MyApp()
                 }
             }
         }
@@ -25,14 +32,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MyApp(names:List<String> = listOf("World","Compose")){
+    Column(modifier = Modifier.padding(vertical = 4.dp)){
+        for(name in names){
+            Greeting(name = name)
+        }
+    }
+}
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)){
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello. ")
+                Text(text = name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text("Show more")
+
+            }
+        }
+//        Column(modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(24.dp)){
+//            Text(text = "Hello.")
+//            Text(text = name)
+//        }
+    }
 }
 
-@Preview(showBackground = true, name = "Text preview")
+
+@Preview(showBackground = true, name = "Text preview",widthDp = 320)
 @Composable
 fun DefaultPreview() {
     JetpackComposeBasicTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
