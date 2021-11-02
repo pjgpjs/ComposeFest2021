28e608fc904bafd63edbc6e332ca88cb66b4b1d9
pjg012 <pjg012@gmail.com>
2021-11-02 16:51:29 +0900

    State in Compose

--- week 1-Jetpack Compose basics/app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
+++ week 1-Jetpack Compose basics/app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,25 +34,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(names:List<String> = listOf("World","Compose")){
-    Column(modifier = Modifier.padding(vertical = 4.dp)){
-        for(name in names){
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
             Greeting(name = name)
         }
     }
 }
+
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary,
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)){
-            Column(modifier = Modifier.weight(1f)) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom =  extraPadding)
+            ) {
                 Text(text = "Hello. ")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text("Show more")
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
 
             }
         }
@@ -58,7 +68,7 @@ fun Greeting(name: String) {
 }
 
 
-@Preview(showBackground = true, name = "Text preview",widthDp = 320)
+@Preview(showBackground = true, name = "Text preview", widthDp = 320)
 @Composable
 fun DefaultPreview() {
     JetpackComposeBasicTheme {
